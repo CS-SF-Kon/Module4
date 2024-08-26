df7124e5d5fb7f6535b75804328d43e3d0acd359
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-08-26 11:57:11 +0700

    Module4: Unit 4.3 - reversing the name

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -7,12 +7,17 @@
             Console.WriteLine("Enter your name:");
             var name = Console.ReadLine();
 
-            Console.WriteLine("Your name contains next letters:");
-            foreach (var lt in name)
-            {
-                Console.Write(lt + " ");
-            }
+            Console.WriteLine("Your name contains next letters in reversed order:");
+            //foreach (var lt in name)
+            //{
+            //    Console.Write(lt + " ");
+            //}
 
+            for (int i = name.Length; i > 0; i--)
+            {
+                Console.Write(name[i - 1] + " ");
+            }
+            
             Console.WriteLine($"The last one letter in your name is {name[name.Length - 1]}");
 
             Console.ReadKey();
